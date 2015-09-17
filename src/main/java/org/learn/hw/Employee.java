947b55a3cee986612a585148f82d3c98ed1970d3
bhaskarv <bhaskarv.online@gmail.com>
2015-09-17 17:42:04 +0530

    Adding employee class

---
+++ src/main/java/org/learn/hw/Employee.java
@@ -0,0 +1,58 @@
+package org.learn.hw;
+
+public class Employee {
+
+	private String name;
+	private int departmentId;
+	
+	public Employee(String name, int departmentId) {
+		super();
+		this.name = name;
+		this.departmentId = departmentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + departmentId;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (departmentId != other.departmentId)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+}
